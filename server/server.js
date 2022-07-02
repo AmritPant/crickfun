0603943b504a9a52f3b00a07313b92cb3ea2f584
Amrit Pant <info.amritpant@gmail.com>
2022-07-02 11:50:42 +0545

    Added connectDatabase Function

--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
 const path = require("path");
-
 require("dotenv").config({
   path: path.join(__dirname, "config", "development.env"),
 });
+
 const app = require("./app");
+const connectDatabase = require("./config/database");
 
 const PORT = process.env.PORT;
+
+connectDatabase();
+
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}..`);
 });
-
-//  amrit/downloads/  --> linux
-//  amrit\downloads  -> window
