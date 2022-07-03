1d01bac411dad5806695a85776f0be91f977d1f2
Amrit Pant <info.amritpant@gmail.com>
2022-07-03 18:25:23 +0545

    Added PlayerId Controller and Search Controller

--- server/controllers/player.controller.js
+++ server/controllers/player.controller.js
@@ -1,4 +1,4 @@
-const Player = require("../models/player.model.js");
+const Player = require('../models/player.model.js');
 
 async function getAllPlayers(req, res) {
   const playersData = await Player.find({});
@@ -10,7 +10,35 @@ async function addNewPlayer(req, res) {
   res.status(201).json(playerData);
 }
 
+async function getOnePlayer(req, res) {
+  const { playerId } = req.params;
+  const curPlayer = await Player.findOne({ _id: playerId });
+
+  if (!curPlayer)
+    return res
+      .status(400)
+      .json({ status: 'failed', message: 'Invalid Player Name' });
+  res.status(200).json({ status: 'success', data: curPlayer });
+}
+
+async function searchPlayer(req, res) {
+  const { player_name } = req.query;
+
+  const playerData = await Player.find({
+    name: /player_name/gi,
+  });
+
+  if (playerData.length === 0) {
+    return res
+      .status(200)
+      .json({ status: 'failed', message: 'Please Enter Correct Name' });
+  }
+  res.status(200).json(playerData);
+}
+
 module.exports = {
   getAllPlayers,
   addNewPlayer,
+  getOnePlayer,
+  searchPlayer,
 };
