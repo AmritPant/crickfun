4931a0d674c64814191c3efacbb8bbe39401b6f7
Amrit Pant <info.amritpant@gmail.com>
2022-07-03 11:18:19 +0545

    Edited API Routes

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
 
 const teamRouter = require('./routes/teamRoute');
 const playerRouter = require('./routes/playerRoute');
@@ -7,13 +7,13 @@ const playerRouter = require('./routes/playerRoute');
 const app = express();
 
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
-app.use('/url/api/v1', teamRouter)
-app.use('/url/api/v1', playerRouter)
+app.use('/api/v1/', teamRouter);
+app.use('/api/v1/', playerRouter);
 
-// app.get("/", (req, res) => {
-//   res.send("<h1> Hello </h1>");
-// });
+app.get('/', (req, res) => {
+  res.send('<h1> Hello </h1>');
+});
 
 module.exports = app;
