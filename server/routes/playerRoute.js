a66219630d2ab28a113d3e5ee40185b28f3a6bbf
Amrit Pant <info.amritpant@gmail.com>
2022-07-03 11:16:08 +0545

    Edited Routes for Player

--- server/routes/playerRoute.js
+++ server/routes/playerRoute.js
@@ -1,9 +1,14 @@
-const express = require("express");
-const playerController = require("../controllers/player.controller");
+const express = require('express');
+const playerController = require('../controllers/player.controller');
 
 const playerRouter = express.Router();
 
-playerRouter.get("/GetAllPlayers", playerController.getAllPlayers);
-playerRouter.post("/AddNewPlayer", playerController.addNewPlayer);
+playerRouter
+  .route('/players')
+  .get(playerController.getAllPlayers)
+  .post(playerController.addNewPlayer);
+
+// playerRouter.get("/player", playerController.getAllPlayers);
+// playerRouter.post("/AddNewPlayer", playerController.addNewPlayer);
 
 module.exports = playerRouter;
