35a5d3fa186543dc39b1c2c2590a0e0d2d1ec75e
Amrit Pant <info.amritpant@gmail.com>
2022-07-04 18:32:31 +0545

    Changed: Players,Batsman,all_rounder,bowlers , not to be required property

--- server/models/team.model.js
+++ server/models/team.model.js
@@ -1,60 +1,66 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const TeamSchema = new mongoose.Schema({
-    year: {
-        type: Number,
-        required: [true, "Please add the year of the team"]
-    },
-    email: {
-        type: String,
-        required: [true, "Please enter the emailID of Team"]
-    },
-    teamName: {
-        type: String,
-        required: [true, "Please add the Name of Team"],
-    },
-    captain: {
-        type: String,
-        required: [true, "Please add the Captain of Team"],
-    },
-    viceCaptain: {
-        type: String,
-        required: [true, "Please add the viceCaptain of Team"],
-    },
-    players: {
-        type: Array,
-        required: [true, "Please add the player names of the Team"],
-    },
-    shareHolder: {
-        type: Array,
-        required: [true, "Please add the share holders of Team"],
-    },
-    rank: {
-        type: Number,
-        required: [true, "Please add the rank of Team"],
-    },
-    Batsman: {
-        type: Array,
-        required: [true, "Please add the batsmans of the team"],
-    },
-    Logo: {
-        type: String,
-        required: [true, "Please add the logo of the team"],
-    },
-    Jersey: {
-        type: String,
-        required: [true, "Please add the jersey of the team"],
-    },
-    Bowler: {
-        type: Array,
-        required: [true, "Please add the bowlers of the team"],
-    },
-    allRounder: {
-        type: Array,
-        required: [true, "Please add the allRounders of the team"],
-    }
-
-
+  year: {
+    type: Number,
+    required: [true, 'Please add the year of the team'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please enter the emailID of Team'],
+  },
+  teamName: {
+    type: String,
+    required: [true, 'Please add the Name of Team'],
+    enum: [
+      'CHENNAI SUPER KINGS',
+      'DELHI CAPITALS',
+      'GUJARAT TITANS',
+      'KOLKATA KNIGHT RIDERS',
+      'LUCKNOW SUPER GIANTS',
+      'MUMBAI INDIANS',
+      'PUNJAB KINGS',
+      'RAJASTHAN ROYALS',
+      'ROYAL CHALLENGERS BANGALORE',
+      'SUNRISERS HYDERABAD',
+    ],
+  },
+  captain: {
+    type: String,
+    required: [true, 'Please add the Captain of Team'],
+  },
+  viceCaptain: {
+    type: String,
+    required: [true, 'Please add the viceCaptain of Team'],
+  },
+  shareHolder: {
+    type: Array,
+    required: [true, 'Please add the share holders of Team'],
+  },
+  rank: {
+    type: Number,
+    required: [true, 'Please add the rank of Team'],
+  },
+  logo: {
+    type: String,
+    required: [true, 'Please add the logo of the team'],
+  },
+  jersey: {
+    type: String,
+    required: [true, 'Please add the jersey of the team'],
+  },
+  players: {
+    type: Array,
+  },
+  batsman: {
+    type: Array,
+  },
+  bowler: {
+    type: Array,
+  },
+  all_rounder: {
+    type: Array,
+  },
 });
 
-module.exports = mongoose.model("Team", TeamSchema);
+module.exports = mongoose.model('Team', TeamSchema);
